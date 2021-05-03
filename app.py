d6966b661602b2bb61c35b5556d8b94c90ccb4c8
Murilo Silva <muriloogps@hotmail.com>
2021-05-02 23:19:35 -0400

    add files
    all the html files should be under "templates" folder

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def get_db_connection():
+    # TODO: Connect to GCP
+    return
+
+@app.route('/')
+def index():
+    # TODO: Call get_db_connection and start connection
+    # TODO: Fetch books read and books to be read from user
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
